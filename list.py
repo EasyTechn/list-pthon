b1cd584d37ff5ef93510dcae248dcef249a74b57
Msanginuhu <nuhumsangi123@gmail.com>
2024-03-26 20:05:19 +0300

    first commit-commiting file

---
+++ list.py
@@ -0,0 +1,14 @@
+my_list=[]
+
+my_list.extend([10,20,30,40])
+
+my_list.insert(1, 15)
+
+my_list.extend([50, 60, 70])
+
+my_list.pop()
+
+index_of_30 = my_list.index(30)
+print('index of value 30:',index_of_30)
+
+print("mylist:", my_list)
